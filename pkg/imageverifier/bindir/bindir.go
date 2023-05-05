70f7ea7d2221a1acd047f783697fd0308d5475d9
Ethan Lowman <ethan.lowman@datadoghq.com>
2023-05-05 15:29:33 -0400

    Add image verifier transfer service plugin system based on a binary directory
    Signed-off-by: Ethan Lowman <ethan.lowman@datadoghq.com>

---
+++ pkg/imageverifier/bindir/bindir.go
@@ -0,0 +1,243 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package bindir
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/containerd/containerd/log"
+	"github.com/containerd/containerd/pkg/imageverifier"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/sirupsen/logrus"
+)
+
+const outputLimitBytes = 1 << 15 // 32 KiB
+
+type Config struct {
+	BinDir             string        `toml:"bin_dir"`
+	MaxVerifiers       int           `toml:"max_verifiers"`
+	PerVerifierTimeout time.Duration `toml:"per_verifier_timeout"`
+}
+
+type ImageVerifier struct {
+	config *Config
+}
+
+var _ imageverifier.ImageVerifier = (*ImageVerifier)(nil)
+
+func NewImageVerifier(c *Config) *ImageVerifier {
+	return &ImageVerifier{
+		config: c,
+	}
+}
+
+func (v *ImageVerifier) VerifyImage(ctx context.Context, name string, desc ocispec.Descriptor) (*imageverifier.Judgement, error) {
+	// os.ReadDir sorts entries by name.
+	entries, err := os.ReadDir(v.config.BinDir)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return &imageverifier.Judgement{
+				OK:     true,
+				Reason: fmt.Sprintf("image verifier directory %v does not exist", v.config.BinDir),
+			}, nil
+		}
+
+		return nil, fmt.Errorf("failed to list directory contents: %w", err)
+	}
+
+	if len(entries) == 0 {
+		return &imageverifier.Judgement{
+			OK:     true,
+			Reason: fmt.Sprintf("no image verifier binaries found in %v", v.config.BinDir),
+		}, nil
+	}
+
+	reason := &strings.Builder{}
+	for i, entry := range entries {
+		if (i+1) > v.config.MaxVerifiers && v.config.MaxVerifiers >= 0 {
+			log.G(ctx).Warnf("image verifiers are being skipped since directory %v has %v entries, more than configured max of %v verifiers", v.config.BinDir, len(entries), v.config.MaxVerifiers)
+			break
+		}
+
+		bin := entry.Name()
+		start := time.Now()
+		exitCode, vr, err := v.runVerifier(ctx, bin, name, desc)
+		runtime := time.Since(start)
+		if err != nil {
+			return nil, fmt.Errorf("failed to call verifier %v (runtime %v): %w", bin, runtime, err)
+		}
+
+		if exitCode != 0 {
+			return &imageverifier.Judgement{
+				OK:     false,
+				Reason: fmt.Sprintf("verifier %v rejected image (exit code %v): %v", bin, exitCode, vr),
+			}, nil
+		}
+
+		if i > 0 {
+			reason.WriteString(", ")
+		}
+		reason.WriteString(fmt.Sprintf("%v => %v", bin, vr))
+	}
+
+	return &imageverifier.Judgement{
+		OK:     true,
+		Reason: reason.String(),
+	}, nil
+}
+
+func (v *ImageVerifier) runVerifier(ctx context.Context, bin string, imageName string, desc ocispec.Descriptor) (exitCode int, reason string, err error) {
+	ctx, cancel := context.WithTimeout(ctx, v.config.PerVerifierTimeout)
+	defer cancel()
+
+	binPath := path.Join(v.config.BinDir, bin)
+	args := []string{
+		"-name", imageName,
+		"-digest", desc.Digest.String(),
+		"-stdin-media-type", ocispec.MediaTypeDescriptor,
+	}
+
+	cmd := exec.CommandContext(ctx, binPath, args...)
+
+	// We construct our own pipes instead of using cmd.StdinPipe, cmd.StoutPipe,
+	// and cmd.StderrPipe in order to set timeouts on reads and writes.
+	stdinRead, stdinWrite, err := os.Pipe()
+	if err != nil {
+		return -1, "", err
+	}
+	cmd.Stdin = stdinRead
+	defer stdinRead.Close()
+	defer stdinWrite.Close()
+
+	stdoutRead, stdoutWrite, err := os.Pipe()
+	if err != nil {
+		return -1, "", err
+	}
+	cmd.Stdout = stdoutWrite
+	defer stdoutRead.Close()
+	defer stdoutWrite.Close()
+
+	stderrRead, stderrWrite, err := os.Pipe()
+	if err != nil {
+		return -1, "", err
+	}
+	cmd.Stderr = stderrWrite
+	defer stderrRead.Close()
+	defer stderrWrite.Close()
+
+	// Close parent ends of pipes on timeout. Without this, I/O may hang in the
+	// parent process.
+	if d, ok := ctx.Deadline(); ok {
+		stdinWrite.SetDeadline(d)
+		stdoutRead.SetDeadline(d)
+		stderrRead.SetDeadline(d)
+	}
+
+	// Fork & exec the child process.
+	if err := cmd.Start(); err != nil {
+		return -1, "", err
+	}
+
+	// Close the child ends of the pipes in the parent process.
+	stdinRead.Close()
+	stdoutWrite.Close()
+	stderrWrite.Close()
+
+	// Write the descriptor to stdin.
+	go func() {
+		// Descriptors are usually small enough to fit in a pipe buffer (which is
+		// often 64 KiB on Linux) so this write usually won't block on the child
+		// process reading stdin. However, synchronously writing to stdin may cause
+		// the parent to block if the descriptor is larger than the pipe buffer and
+		// the child process doesn't read stdin. Therefore, we write to stdin
+		// asynchronously, limited by the stdinWrite deadline set above.
+		err := json.NewEncoder(stdinWrite).Encode(desc)
+		if err != nil {
+			// This may error out with a "broken pipe" error if the descriptor is
+			// larger than the pipe buffer and the child process does not read all
+			// of stdin.
+			log.G(ctx).WithError(err).Warn("failed to completely write descriptor to stdin")
+		}
+		stdinWrite.Close()
+	}()
+
+	// Pipe verifier stderr lines to debug logs.
+	stderrLog := log.G(ctx).Logger.WithFields(logrus.Fields{
+		"image_verifier": bin,
+		"stream":         "stderr",
+	})
+	stderrLogDone := make(chan struct{})
+	go func() {
+		defer close(stderrLogDone)
+		lr := &io.LimitedReader{
+			R: stderrRead,
+			N: outputLimitBytes,
+		}
+		s := bufio.NewScanner(lr)
+		for s.Scan() {
+			// Log a line of stderr, appending a message if it is truncated.
+			line := strings.Builder{}
+			line.WriteString(s.Text())
+			if lr.N == 0 {
+				// LimitedReader finished reading stderr.
+				// Peek ahead to see if stderr reader was truncated.
+				b := make([]byte, 1)
+				if n, _ := stderrRead.Read(b); n > 0 {
+					line.WriteString("(stderr truncated)")
+				}
+			}
+			stderrLog.Debug(line.String())
+		}
+		if err := s.Err(); err != nil {
+			stderrLog.WithError(err).Debug("error logging image verifier stderr")
+		}
+	}()
+
+	stdout, err := io.ReadAll(io.LimitReader(stdoutRead, outputLimitBytes))
+	if err != nil {
+		return -1, "", fmt.Errorf("reading verifier stdout: %w", err)
+	}
+
+	m := strings.Builder{}
+	m.WriteString(strings.TrimSpace(string(stdout)))
+	// Peek ahead to see if stdout is truncated.
+	b := make([]byte, 1)
+	if n, _ := stdoutRead.Read(b); n > 0 {
+		m.WriteString("(stdout truncated)")
+	}
+	reason = m.String()
+
+	<-stderrLogDone
+	if err := cmd.Wait(); err != nil {
+		if ee := (&exec.ExitError{}); errors.As(err, &ee) && ee.ProcessState.Exited() {
+			return ee.ProcessState.ExitCode(), reason, nil
+		}
+		return -1, "", err
+	}
+
+	return cmd.ProcessState.ExitCode(), reason, nil
+}
---
+++ pkg/imageverifier/bindir/bindir_test.go
@@ -0,0 +1,417 @@
+//go:build !windows
+
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package bindir
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+	"testing"
+	"time"
+
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Currently, these tests don't run on Windows due to dependency on Bash.
+
+func newBinDir(t *testing.T, scripts ...string) string {
+	t.Helper()
+	binDir := t.TempDir()
+
+	for i, s := range scripts {
+		err := os.WriteFile(
+			path.Join(binDir, fmt.Sprintf("%v.sh", i)),
+			[]byte(strings.TrimSpace(s)),
+			0700,
+		)
+		require.NoError(t, err)
+	}
+
+	return binDir
+}
+
+func TestBinDirVerifyImage(t *testing.T) {
+	t.Run("proper input/output management", func(t *testing.T) {
+		outDir := t.TempDir()
+		argsFile := path.Join(outDir, "args.txt")
+		stdinFile := path.Join(outDir, "stdin.txt")
+
+		binDir := newBinDir(t, fmt.Sprintf(`
+#!/usr/bin/env bash
+set -euf -o pipefail
+echo -n $@ > %v
+cat - > %v
+echo Reason A line 1
+echo Debug A line 1 1>&2
+echo Reason A line 2
+echo Debug A line 2 1>&2
+exit 0
+			`, argsFile, stdinFile),
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       -1,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{
+			Digest:      "sha256:98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4",
+			MediaType:   "application/vnd.docker.distribution.manifest.list.v2+json",
+			Size:        2048,
+			Annotations: map[string]string{"a": "b"},
+		})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.Equal(t, "0.sh => Reason A line 1\nReason A line 2", j.Reason)
+
+		b, err := os.ReadFile(argsFile)
+		require.NoError(t, err)
+		assert.Equal(t, "-name registry.example.com/image:abc -digest sha256:98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4 -stdin-media-type application/vnd.oci.descriptor.v1+json", string(b))
+
+		b, err = os.ReadFile(stdinFile)
+		require.NoError(t, err)
+		assert.Equal(t, `{"mediaType":"application/vnd.docker.distribution.manifest.list.v2+json","digest":"sha256:98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4","size":2048,"annotations":{"a":"b"}}`, strings.TrimSpace(string(b)))
+	})
+
+	t.Run("stdout truncation", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+set -euf -o pipefail
+head -c 50000 /dev/random
+exit 0
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       -1,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.Less(t, len(j.Reason), outputLimitBytes+1024)
+	})
+
+	t.Run("missing directory", func(t *testing.T) {
+		v := NewImageVerifier(&Config{
+			BinDir:             path.Join(t.TempDir(), "missing_directory"),
+			MaxVerifiers:       10,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.NotEmpty(t, j.Reason)
+	})
+
+	t.Run("empty directory", func(t *testing.T) {
+		v := NewImageVerifier(&Config{
+			BinDir:             t.TempDir(),
+			MaxVerifiers:       10,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.NotEmpty(t, j.Reason)
+	})
+
+	t.Run("max verifiers = 0", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+# This isn't called.
+exit 1
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       0,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.Empty(t, j.Reason)
+	})
+
+	t.Run("max verifiers = 1", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+# This isn't called.
+exit 1
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       1,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.NotEmpty(t, j.Reason)
+	})
+
+	t.Run("max verifiers = 2", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+# This isn't called.
+exit 1
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       2,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.NotEmpty(t, j.Reason)
+	})
+
+	t.Run("max verifiers = 2", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+# This isn't called.
+exit 1
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       2,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.NotEmpty(t, j.Reason)
+	})
+
+	t.Run("max verifiers = 3, all accept", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+echo Reason A
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason B
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason C
+exit 0
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       3,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.Equal(t, "0.sh => Reason A, 1.sh => Reason B, 2.sh => Reason C", j.Reason)
+	})
+
+	t.Run("max verifiers = 3, with reject", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+echo Reason A
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason B
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason C
+exit 1
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       3,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.False(t, j.OK)
+		assert.Equal(t, "verifier 2.sh rejected image (exit code 1): Reason C", j.Reason)
+	})
+
+	t.Run("max verifiers = -1, all accept", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+echo Reason A
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason B
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason C
+exit 0
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       -1,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.True(t, j.OK)
+		assert.Equal(t, "0.sh => Reason A, 1.sh => Reason B, 2.sh => Reason C", j.Reason)
+	})
+
+	t.Run("max verifiers = -1, with reject", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+echo Reason A
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason B
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+echo Reason C
+exit 1
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       -1,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.NoError(t, err)
+		assert.False(t, j.OK)
+		assert.Equal(t, "verifier 2.sh rejected image (exit code 1): Reason C", j.Reason)
+	})
+
+	t.Run("max verifiers = -1, with timeout", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+# Greater than 250ms PerVerifierTimeout.
+sleep 1000
+exit 0
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       -1,
+			PerVerifierTimeout: 250 * time.Millisecond,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.Error(t, err)
+		assert.Nil(t, j)
+	})
+
+	t.Run("max verifiers = -1, with exec failure", func(t *testing.T) {
+		binDir := newBinDir(t, `
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/usr/bin/env bash
+exit 0
+			`,
+			`
+#!/badshell
+exit 0
+			`,
+		)
+
+		v := NewImageVerifier(&Config{
+			BinDir:             binDir,
+			MaxVerifiers:       -1,
+			PerVerifierTimeout: 1 * time.Second,
+		})
+
+		j, err := v.VerifyImage(context.Background(), "registry.example.com/image:abc", ocispec.Descriptor{})
+		assert.Error(t, err)
+		assert.Nil(t, j)
+	})
+}
